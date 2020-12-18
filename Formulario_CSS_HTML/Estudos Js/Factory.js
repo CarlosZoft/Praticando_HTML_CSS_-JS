d20fdbac06b1be7098962f75be7d4be6576617d3
Carlos Rafael <carlosoftwar@gmail.com>
2020-12-18 03:00:50 +0000

    factory

---
+++ Formulario_CSS_HTML/Estudos Js/Factory.js
@@ -0,0 +1,13 @@
+function criaCarros(marca, cavalos = 200, modelo){
+    return {
+      marca ,
+        cavalos,
+          modelo 
+    }
+}
+
+
+
+console.log(criaCarros('Volkswagem' ,400 ,'sedan'));
+console.log(criaCarros('Fiat',  235,  '4x4')); 
+
