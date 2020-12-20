8fa188053b768c6e4561b201f2b7a7d83bf50436
Carlos Rafael <carlosoftwar@gmail.com>
2020-12-20 03:13:33 +0000

    Filtro Numbers

---
+++ Estudos Js/Exercicios-Fundamentos de Programação/filtraNumeros.js
@@ -0,0 +1,11 @@
+/*Crie uma função que receba um array de elementos e retorne um array somente com os números presentes no
+array recebido como parâmetro.
+*/
+
+const filtrarNumeros = (a) =>{
+    number = (b) => typeof b == typeof 1 
+    return a.filter(number)
+}
+console.log(filtrarNumeros(["Javascript", 1, "3", "Web", 20])) 
+
+console.log(filtrarNumeros(["a", "c"]))
